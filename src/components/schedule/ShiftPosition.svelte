a45749923662eb498a535f3a0db8ddc06e84ea44
Dwight VanTuyl <dvantuyl@gmail.com>
2022-03-19 00:55:59 -0700

    Schedule | Glob together overlaping job shifts

--- src/components/schedule/ShiftPosition.svelte
+++ src/components/schedule/ShiftPosition.svelte
@@ -5,18 +5,29 @@
   export let date: string;
   export let job: Job;
   export let col: number;
-  export let shiftId: string;
+  export let from: number;
+  export let to: number;
   export let signUps: ShiftSignUp[];
 
-  const shift = job.shifts[shiftId];
-  const [fromHours, fromMinutes] = shift.from
-    .split(':')
-    .map((v) => parseInt(v));
-  const [toHours, toMinutes] = shift.to.split(':').map((v) => parseInt(v));
-  const rowStart = 2 + fromHours * 12 + Math.round(fromMinutes / 5);
-  const rowEnd = 2 + toHours * 12 + Math.round(toMinutes / 5) - rowStart;
+  const fromDate = new Date(from);
+  const toDate = new Date(to);
+
+  //const shift = job.shifts[shiftId];
+  // const [fromHours, fromMinutes] = shift.from
+  //   .split(':')
+  //   .map((v) => parseInt(v));
+  // const [toHours, toMinutes] = shift.to.split(':').map((v) => parseInt(v));
+  const rowStart =
+    2 + fromDate.getHours() * 12 + Math.round(fromDate.getMinutes() / 5);
+  const rowEnd =
+    2 + toDate.getHours() * 12 + Math.round(toDate.getMinutes() / 5) - rowStart;
   const color = job.color;
-  const time = shift.from;
+  const time = fromDate.toLocaleTimeString([], {
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  // TODO: this assumes that all shifts in this glob have same start time
 </script>
 
 <li
--- src/components/schedule/Shifts.svelte
+++ src/components/schedule/Shifts.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Actions from '$components/event/Actions.svelte';
   import type { Job, ShiftSignUp } from '$models';
   import ShiftPosition from './ShiftPosition.svelte';
 
@@ -8,8 +9,32 @@
   export let jobSignUps: { [shiftId: string]: ShiftSignUp[] };
 
   const shiftSignUps = Object.entries(jobSignUps);
+  const mergedShiftSignUps = shiftSignUps.reduce((acc, [shiftId, signUps]) => {
+    const shift = job.shifts[shiftId];
+    const from = Date.parse('0001-01-01T' + shift.from);
+    const to = Date.parse('0001-01-01T' + shift.to);
+
+    const newGlobIndex = acc.findIndex((glob) => {
+      return glob.from <= from && glob.to >= to;
+    });
+
+    if (newGlobIndex > -1) {
+      const newGlob = acc[newGlobIndex];
+      newGlob.from = Math.min(newGlob.from, from);
+      newGlob.to = Math.max(newGlob.to, to);
+      newGlob.signUps = [...newGlob.signUps, ...signUps];
+      acc[newGlobIndex] = newGlob;
+    } else {
+      acc.push({
+        from,
+        to,
+        signUps,
+      });
+    }
+    return acc;
+  }, [] as Array<{ from: number; to: number; signUps: ShiftSignUp[] }>);
 </script>
 
-{#each shiftSignUps as signUps}
-  <ShiftPosition {date} {job} {col} shiftId={signUps[0]} signUps={signUps[1]} />
+{#each mergedShiftSignUps as shiftSignUps}
+  <ShiftPosition {date} {job} {col} {...shiftSignUps} />
 {/each}
