ee289f2b14aee59a9e490a1a505e1bbf4200d933
Dwight VanTuyl <dvantuyl@gmail.com>
2022-03-18 23:24:36 -0700

    Sidebar | fix active selection missfires

--- src/components/sidebar/SidebarItem.svelte
+++ src/components/sidebar/SidebarItem.svelte
@@ -20,7 +20,7 @@
   }
 </script>
 
-{#if shouldDisplayRoute && isCurrent()}
+{#if pathname && shouldDisplayRoute && isCurrent()}
   <a
     {href}
     class="bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
