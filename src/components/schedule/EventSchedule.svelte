8aee4638b24e0668f7719c5afc494c170a41224a
Dwight VanTuyl <dvantuyl@gmail.com>
2022-03-26 23:23:38 -0700

    Schedule | Mobile swiping

--- src/components/schedule/EventSchedule.svelte
+++ src/components/schedule/EventSchedule.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { JobSignUpCollection } from '$types';
+  import { createEventDispatcher } from 'svelte';
   import { VEvent } from '$models';
   import HorizontalLines from './HorizontalLines.svelte';
   import JobsXAxis from './JobsXAxis.svelte';
@@ -18,10 +19,58 @@
   $: date = event.date;
   $: [startTime, endTime] = event.roundedTimeRange || [0, 0];
 
+  const dispatch = createEventDispatcher();
+
   let mobileJob = 0;
+  let touchstartX = 0;
+  let touchstartY = 0;
+  let touchendX = 0;
+  let touchendY = 0;
+
+  function handleGesture(touchstartX, touchstartY, touchendX, touchendY) {
+    const delx = touchendX - touchstartX;
+    const dely = touchendY - touchstartY;
+    if (Math.abs(delx) > Math.abs(dely)) {
+      if (delx > 0) return 'right';
+      else return 'left';
+    } else if (Math.abs(delx) < Math.abs(dely)) {
+      if (dely > 0) return 'down';
+      else return 'up';
+    } else return 'tap';
+  }
+
+  function handleTouchStart(event: TouchEvent) {
+    touchstartX = event.touches[0].clientX;
+    touchstartY = event.touches[0].clientY;
+  }
+
+  function handleTouchEnd(event: TouchEvent) {
+    touchendX = event.changedTouches[0].clientX;
+    touchendY = event.changedTouches[0].clientY;
+    const direction = handleGesture(
+      touchstartX,
+      touchstartY,
+      touchendX,
+      touchendY
+    );
+    console.log('direction', direction);
+    if (direction === 'left' && mobileJob < jobs.length - 1) {
+      mobileJob = mobileJob + 1;
+    } else if (direction === 'left' && allowNextPage) {
+      dispatch('nextschedulepage');
+    } else if (direction === 'right' && mobileJob > 0) {
+      mobileJob = mobileJob - 1;
+    } else if (direction === 'right' && allowPrevPage) {
+      dispatch('prevschedulepage');
+    }
+  }
 </script>
 
-<div class="flex flex-auto flex-col  bg-white">
+<div
+  class="flex flex-auto flex-col  bg-white"
+  on:touchstart={handleTouchStart}
+  on:touchend={handleTouchEnd}
+>
   <div
     style="width: 165%;"
     class="flex max-w-full flex-none flex-col sm:max-w-none md:max-w-full"
