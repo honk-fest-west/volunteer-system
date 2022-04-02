073cbcd91df5ff02b1bea68fa7ab99d654c9921c
Dwight VanTuyl <dvantuyl@gmail.com>
2022-04-01 23:25:08 -0700

    Volunteers | Email Selected

---
+++ src/components/volunteer/VolunteerEmail.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  export let emailAddresses: string[] = [];
+
+  const volunteerEmail = 'volunteer@honkfestwest.org';
+</script>
+
+<a
+  class="bg-pink-500 hover:bg-pink-700 text-white font-semibold py-2 px-4 rounded flex"
+  href="mailto:{volunteerEmail}?bcc={emailAddresses.join(',')}"
+>
+  <div class="mr-2">
+    <svg
+      role="img"
+      aria-labelledby="email"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+    >
+      <title id="email">Email</title>
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+      />
+    </svg>
+  </div>
+  <span class="block">Selected</span>
+</a>
+
+<!-- <button
+class="bg-pink-500 hover:bg-pink-700 text-white font-semibold py-2 px-4 rounded flex"
+on:click={() => send('AT.CREATE')}
+>
+<div class="mr-2">
+	<svg
+		role="img"
+		aria-labelledby="email"
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-6 w-6"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<title id="email">Email</title>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+		/>
+	</svg>
+</div>
+<span class="block">Selected</span>
+</button> -->
--- src/routes/system/volunteers/index.svelte
+++ src/routes/system/volunteers/index.svelte
@@ -6,11 +6,34 @@
   import TableHead from '$components/table/TableHead.svelte';
   import TableRow from '$components/table/TableRow.svelte';
   import TableCell from '$components/table/TableCell.svelte';
+  import VolunteerEmail from '$components/volunteer/VolunteerEmail.svelte';
   import { getContext, onMount } from 'svelte';
   import { formatPhoneNumber } from '$util';
   const { state, send } = getContext('volunteerMachine');
 
   $: volunteers = $state.context.volunteers as User[];
+  $: allSelected = emailAddresses?.length === allEmailAddresses?.length;
+  $: allEmailAddresses = volunteers.map(
+    (volunteer) => volunteer.email
+  ) as string[];
+
+  let emailAddresses: string[] = [];
+
+  $: selectAll = () => {
+    if (emailAddresses.length === allEmailAddresses.length) {
+      emailAddresses = [];
+    } else {
+      emailAddresses = allEmailAddresses;
+    }
+  };
+
+  $: selectVolunteerEmail = (email: string) => {
+    if (emailAddresses.includes(email)) {
+      emailAddresses = emailAddresses.filter((e) => e !== email);
+    } else {
+      emailAddresses = [...emailAddresses, email];
+    }
+  };
 
   onMount(() => {
     send('AT.INDEX');
@@ -18,48 +41,70 @@
 </script>
 
 <div in:fade={{ duration: 100 }}>
-  <MainContainer>
-    <h1 slot="header" class="text-3xl font-extrabold text-pink-700 grow">
-      Volunteers
-    </h1>
-    <TableContainer>
-      <TableRow slot="head">
-        <TableHead side="left" text="left">Name</TableHead>
-        <TableHead text="right">Telephone</TableHead>
-        <TableHead side="right">Status</TableHead>
-      </TableRow>
+  {#key emailAddresses}
+    <MainContainer>
+      <h1 slot="header" class="text-3xl font-extrabold text-pink-700 grow">
+        Volunteers
+      </h1>
+      <div slot="actions">
+        <VolunteerEmail {emailAddresses} />
+      </div>
+      <TableContainer>
+        <TableRow slot="head">
+          <TableHead side="left" text="left">Name</TableHead>
+          <TableHead text="right">Telephone</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead side="right">
+            <label for="emailAddresses">Email</label>
 
-      {#each volunteers as volunteer}
-        <TableRow>
-          <TableCell>
-            <div class="flex items-center">
-              <div class="h-10 w-10 flex-shrink-0">
-                <img
-                  class="h-10 w-10 rounded-full"
-                  src={volunteer.photoURL}
-                  alt=""
-                />
-              </div>
-              <div class="ml-4">
-                <div class="font-medium text-gray-900">
-                  {volunteer.displayName}
-                </div>
-                <div class="text-indigo-500 cursor-pointer hover:underline">
-                  {volunteer.email}
-                </div>
-              </div>
-            </div>
-          </TableCell>
-          <TableCell text="right">
-            <a
-              class="text-indigo-500 hover:underline sm:hover:no-underline md:text-gray-500 md:cursor-text"
-              href={`tel:${volunteer.phoneNumber}`}
-              >{formatPhoneNumber(volunteer.phoneNumber)}</a
-            ></TableCell
-          >
-          <TableCell>{volunteer.status}</TableCell>
+            <input
+              id="emailAddresses"
+              type="checkbox"
+              checked={allSelected}
+              on:change={() => selectAll()}
+            />
+          </TableHead>
         </TableRow>
-      {/each}
-    </TableContainer>
-  </MainContainer>
+
+        {#each volunteers as volunteer}
+          <TableRow>
+            <TableCell>
+              <div class="flex items-center">
+                <div class="h-10 w-10 flex-shrink-0">
+                  <img
+                    class="h-10 w-10 rounded-full"
+                    src={volunteer.photoURL}
+                    alt=""
+                  />
+                </div>
+                <div class="ml-4">
+                  <div class="font-medium text-gray-900">
+                    {volunteer.displayName}
+                  </div>
+                  <div class="text-indigo-500 cursor-pointer hover:underline">
+                    {volunteer.email}
+                  </div>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell text="right">
+              <a
+                class="text-indigo-500 hover:underline sm:hover:no-underline md:text-gray-500 md:cursor-text"
+                href={`tel:${volunteer.phoneNumber}`}
+                >{formatPhoneNumber(volunteer.phoneNumber)}</a
+              ></TableCell
+            >
+            <TableCell>{volunteer.status}</TableCell>
+            <TableCell>
+              <input
+                type="checkbox"
+                checked={emailAddresses.includes(volunteer.email)}
+                on:change={() => selectVolunteerEmail(volunteer.email)}
+              />
+            </TableCell>
+          </TableRow>
+        {/each}
+      </TableContainer>
+    </MainContainer>
+  {/key}
 </div>
